436ad738ab8f532bc62aa52d2155636e0e220773
Leturgone <finekon221@gmail.com>
2024-09-09 15:48:31 +0300

    Добавлены фрагменты

--- work2/app/src/main/java/com/example/work2/MainActivity.kt
+++ work2/app/src/main/java/com/example/work2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.work2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.work2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Объявление переменной для привязки к разметке
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Получение корневого представления разметки
+        val view = binding.root
+        // Установка корневого представления разметки в качестве контента активности
+        setContentView(view)
+
+        binding.mybutton.setOnClickListener{
+            binding.textView.text = "Текст"
+        }
     }
 }
