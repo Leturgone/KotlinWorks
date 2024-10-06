58de738cdf4ee00b0934e53540ac390cffaa4b0e
Leturgone <finekon221@gmail.com>
2024-10-06 14:32:23 +0300

    Перенесена 7я

--- work8/app/build.gradle.kts
+++ work8/app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding= true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
