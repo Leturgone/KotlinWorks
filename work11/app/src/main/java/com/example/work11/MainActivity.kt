02d2f36ddd33d366e80a8ee5dcbcbd87cb309e32
Leturgone <finekon221@gmail.com>
2024-10-23 00:41:24 +0300

    Сделано

--- work11/app/src/main/java/com/example/work11/MainActivity.kt
+++ work11/app/src/main/java/com/example/work11/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,35 +18,55 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,11 +78,13 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import java.io.File
 import java.net.URL
 import java.util.Date
+
 
 class MainActivity : ComponentActivity() {
 
@@ -69,7 +92,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Work11Theme {
-
+                Menu()
             }
         }
     }
@@ -78,38 +101,73 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Menu(){
+        var currentScreen by remember { mutableIntStateOf(1) }
+        var imageList = remember { mutableStateListOf<String>() }
+        var title by remember {mutableStateOf("Главная") }
+        val scope = rememberCoroutineScope()
+
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                // Содержимое бокового меню
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(16.dp)
+                ) {
+                    IconButton(onClick = {currentScreen = 1
+                        title = "Главная"}){
+                        Icon(Icons.Filled.Home, contentDescription =null, tint = Color.White)
+                    }
+                    Spacer(Modifier.padding(60.dp))
+                    IconButton(onClick = {currentScreen = 2
+                        title = "Список"}){
+                        Icon(Icons.Filled.List, contentDescription =null, tint = Color.White)
+                    }
+                }
+            }
+        ){
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text("Приложение с изображениями") },
+                    colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+                    title = { Text(title) },
                     navigationIcon = {
-                        IconButton(onClick = { /* TODO: Handle menu click */ }) {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
+                        }) {
                             Icon(Icons.Filled.Menu, contentDescription = "Menu")
                         }
                     }
                 )
-            }
-            , bottomBar = {
-                BottomAppBar(CircleShape
+            }, bottomBar = {
+//                NavigationBar(containerColor = MaterialTheme.colorScheme.primary) {
+//                    NavigationBarItem(colors = NavigationBarItemDefaults.colors(
+//                        unselectedIconColor = Color.White),
+//                        selected = false, onClick = {currentScreen = 1
+//                                                                  title = "Главная"}, icon = {Icon(Icons.Filled.Home, contentDescription = null)})
+//                    NavigationBarItem(colors = NavigationBarItemDefaults.colors( unselectedIconColor = Color.White),
+//                        selected = false, onClick = {currentScreen = 2
+//                                                                  title = "Список"}, icon = { Icon(Icons.Filled.List,contentDescription = null)})
+//                }
+                BottomAppBar(containerColor = MaterialTheme.colorScheme.primary
                 ) {
-                    IconButton(onClick = { /* Handle icon click */ })
-                    {
-                        Icon(Icons.Filled.Menu, contentDescription =
-                        "Menu")
+                    IconButton(onClick = {currentScreen = 1
+                        title = "Главная"}){
+                        Icon(Icons.Filled.Home, contentDescription =null, tint = Color.White)
                     }
                     Spacer(Modifier.weight(1f, true))
-                    IconButton(onClick = { /* Handle icon click */ })
-                    {
-                        Icon(Icons.Filled.Favorite, contentDescription
-                        = "Favorite")
+                    IconButton(onClick = {currentScreen = 2
+                        title = "Список"}){
+                        Icon(Icons.Filled.List, contentDescription =null, tint = Color.White)
                     }
-                    IconButton(onClick = { /* Handle icon click */ })
-                    {
-                        Icon(Icons.Filled.Share, contentDescription =
-                        "Share")
-                    }
+
                 }
-            }) {
+            }
+            ) {
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
@@ -124,21 +182,24 @@ class MainActivity : ComponentActivity() {
                                 .fillMaxWidth()
                                 .padding(16.dp)
                         ) {
-
-                            Greeting("Android")
+                            when(currentScreen){
+                                1 -> Greeting(imageList = imageList)
+                                2 -> ListScreen(imageList = imageList)
+                            }
                         }
                     }
                 }
             }
         }
+        }
 
     }
+
     @SuppressLint("MutableCollectionMutableState", "UnusedMaterial3ScaffoldPaddingParameter")
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
+    fun Greeting(imageList:MutableList<String>) {
         var text by remember { mutableStateOf("") }
         var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
-        var imageList = remember { mutableStateListOf<String>() }
 
 
         Column(
@@ -184,6 +245,12 @@ class MainActivity : ComponentActivity() {
             }) {
                 Text("Загрузть изображение")
             }
+        }
+    }
+    @Composable
+    fun ListScreen(imageList:MutableList<String>){
+        Column(Modifier.padding(50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally){
             LazyColumn {
                 items(imageList) { item ->
                     Row {
@@ -201,6 +268,7 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+
         }
     }
     fun downloadImageByURl(imageUrl: String): Bitmap? {
@@ -260,12 +328,4 @@ class MainActivity : ComponentActivity() {
         return deferred.await()
     }
 
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        Work11Theme {
-            Greeting("Android")
-        }
-    }
 }
