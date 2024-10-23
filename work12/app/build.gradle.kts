1907dc0ac5dcf3dddb3a9862473c4d3f938fa6f5
Leturgone <finekon221@gmail.com>
2024-10-23 11:40:33 +0300

    Сделано

--- work12/app/build.gradle.kts
+++ work12/app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.2")
